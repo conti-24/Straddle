00d24f7f7d13c1afa4ef0fd233ed57f6305211d5
Ivan Conti <ivanconti24@gmail.com>
2023-04-30 01:42:08 -0300

    csv2xlsx

--- Convert.py
+++ Convert.py
@@ -1,19 +1,17 @@
 import pandas as pd
 
-print('Converting .CSV to .XLSX File...')
+print('\nConverting .CSV to .XLSX File...')
 
 cont_1 = [1,2,3,4,5,6,7,8,9]
 cont_2 = [10,11,12]
 
 for i in cont_1:
     readfile = pd.read_csv(f'ETHUSDT-1h-2022-0{i}.csv')
-    readfile.to_excel(f'ETHUSDT-1h-2022-0{i}.xlsx', 
-                      index=None, header = True , startcol=0 , startrow=1)
+    readfile.to_excel(f'ETHUSDT-1h-2022-0{i}.xlsx', index=None, header = True , startcol=0 , startrow=1)
     
 for i in cont_2:
     readfile = pd.read_csv(f'ETHUSDT-1h-2022-{i}.csv')
-    readfile.to_excel(f'ETHUSDT-1h-2022-{i}.xlsx', 
-                      index=None, header = True , startcol=0 , startrow=1)
+    readfile.to_excel(f'ETHUSDT-1h-2022-{i}.xlsx', index=None, header = True , startcol=0 , startrow=1)
 
 print('Formatting XLSX file...')
 
@@ -23,7 +21,7 @@ for i in cont_1:
     data.set_index('date', inplace=True)
     data.drop(['F','G','H','I','J','K','L'],axis=1,inplace=True)
     data.index = pd.to_datetime(data.index, unit='ms')
-    data.to_excel(f'ETHUSDT-1h-2023-0{i}.xlsx')
+    data.to_excel(f'ETHUSDT-1h-2022-0{i}.xlsx')
 
 for i in cont_2:
     data = pd.read_excel(f'ETHUSDT-1h-2022-{i}.xlsx')
@@ -31,22 +29,22 @@ for i in cont_2:
     data.set_index('date', inplace=True)
     data.drop(['F','G','H','I','J','K','L'],axis=1,inplace=True)
     data.index = pd.to_datetime(data.index, unit='ms')
-    data.to_excel(f'ETHUSDT-1h-2023-{i}.xlsx')
+    data.to_excel(f'ETHUSDT-1h-2022-{i}.xlsx')
 
 print('Concatenating...')
 
-d1 = pd.read_excel('ETHUSDT-1h-2023-01.xlsx')
-d2 = pd.read_excel('ETHUSDT-1h-2023-02.xlsx')
-d3 = pd.read_excel('ETHUSDT-1h-2023-03.xlsx')
-d4 = pd.read_excel('ETHUSDT-1h-2023-04.xlsx')
-d5 = pd.read_excel('ETHUSDT-1h-2023-05.xlsx')
-d6 = pd.read_excel('ETHUSDT-1h-2023-06.xlsx')
-d7 = pd.read_excel('ETHUSDT-1h-2023-07.xlsx')
-d8 = pd.read_excel('ETHUSDT-1h-2023-08.xlsx')
-d9 = pd.read_excel('ETHUSDT-1h-2023-09.xlsx')
-d10 = pd.read_excel('ETHUSDT-1h-2023-10.xlsx')
-d11 = pd.read_excel('ETHUSDT-1h-2023-11.xlsx')
-d12 = pd.read_excel('ETHUSDT-1h-2023-12.xlsx')
+d1 = pd.read_excel('ETHUSDT-1h-2022-01.xlsx')
+d2 = pd.read_excel('ETHUSDT-1h-2022-02.xlsx')
+d3 = pd.read_excel('ETHUSDT-1h-2022-03.xlsx')
+d4 = pd.read_excel('ETHUSDT-1h-2022-04.xlsx')
+d5 = pd.read_excel('ETHUSDT-1h-2022-05.xlsx')
+d6 = pd.read_excel('ETHUSDT-1h-2022-06.xlsx')
+d7 = pd.read_excel('ETHUSDT-1h-2022-07.xlsx')
+d8 = pd.read_excel('ETHUSDT-1h-2022-08.xlsx')
+d9 = pd.read_excel('ETHUSDT-1h-2022-09.xlsx')
+d10 = pd.read_excel('ETHUSDT-1h-2022-10.xlsx')
+d11 = pd.read_excel('ETHUSDT-1h-2022-11.xlsx')
+d12 = pd.read_excel('ETHUSDT-1h-2022-12.xlsx')
 data = pd.concat([d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12], ignore_index=True)
 
 data.to_excel('2022.xlsx')
